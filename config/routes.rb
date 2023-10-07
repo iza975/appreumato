07d910a19d35dcefd0424d4ffd516805c8662ba4
EduardoFBregenski <eduardofbregenski@gmail.com>
2023-10-07 20:12:23 -0300

    correcting small errors

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,19 @@
 Rails.application.routes.draw do
+  get 'password_resets/new'
+  get 'password_resets/create'
   get 'login', to: 'sessions#new'
   post 'login', to: 'sessions#create'
 
+  get 'password_reset', to: 'password_resets#new'
+  post 'password_reset', to: 'password_resets#create'
+  
+  # Rotas para redefinição de senha
+  get 'password_resets/new', to: 'password_resets#new', as: 'new_password_reset'
+  post 'password_resets', to: 'password_resets#create'
+
+  get 'password_resets/edit', to: 'password_resets#edit', as: 'edit_password_reset'
+  patch 'password_resets/update', to: 'password_resets#update'
+  
 
   delete '/logout', to: 'sessions#destroy', as: 'logout'
   get '/logout', to: 'sessions#destroy'
